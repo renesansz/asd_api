ab793722b7e097d1ae8bc80812f2687970d0eb48
Rene Padillo <padillo.renemari@gmail.com>
2021-05-07 14:40:17 +0800

    feat: added type query param filter for Entry
    Added `type` query param for filtering Entry list by `entry_type` during fetch request.
    
    Signed-off-by: Rene Padillo <padillo.renemari@gmail.com>

--- manifesto/views.py
+++ manifesto/views.py
@@ -15,6 +15,13 @@ class EntryViewSet(viewsets.ModelViewSet):
     queryset = Entry.objects.all()
     serializer_class = EntrySerializer
 
+    def get_queryset(self):
+        queryset = Entry.objects.all()
+        type = self.request.query_params.get('type')
+        if type is not None:
+            queryset = queryset.filter(entry_type__id=type)
+        return queryset
+
 class EntryTypeViewSet(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication, SessionAuthentication, BasicAuthentication]
     permission_classes = (IsAuthenticated,)
